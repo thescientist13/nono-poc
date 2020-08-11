4f6f4764b5e852bfa4e6730e184e343ab91f619e
Owen Buckley <owenbuckley13@gmail.com>
2020-08-10 21:28:39 -0400

    lit element with unpkg

---
+++ src/components/counter.component.js
@@ -0,0 +1,46 @@
+import { LitElement, html } from 'https://unpkg.com/lit-element@2.3.1/lit-element.js?module';
+
+class MyCounter extends LitElement {
+
+  constructor() {
+    super();
+
+    this.count = 0;
+  };
+
+  static get properties() {
+    return {
+      count: {
+        type: Number
+      }
+    }
+  }
+
+  increment() {
+    console.log('increment!');
+    this.count++;
+  }
+
+  decrement() {
+    console.log('decrement!');
+    this.count--;
+  }
+
+  render() {
+    console.log('render', this.count);
+    const { count } = this;
+
+    return html`
+      <div>
+        <h1>My Counter</h1>
+        
+        <p>The current count is: ${count}</p>
+        
+        <button @click="${this.increment}">+</button>
+        <button @click="${this.decrement}">-</button>
+      </div>
+    `;
+  }
+}
+
+customElements.define('my-counter', MyCounter);
